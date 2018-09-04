343e8505da7e20050d75f9c3875898bcd97d3187
wyjzxl <36919429+wyjzxl@users.noreply.github.com>
2018-09-04 19:27:25 +0800

    haha
    best

---
+++ print tuxing/src/dayintuxing.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class dayintuxing {
+    public static void main(String[] args) {
+        int a=0;
+        System.out.print("请输入行数:");
+
+        Scanner wyj=new Scanner(System.in);
+        a=wyj.nextInt();
+        for(int i=1;i<=a;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
