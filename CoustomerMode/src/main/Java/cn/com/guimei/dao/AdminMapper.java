0d6dfc4f7a5a5c61aeeb94caad1c6f9efcbd6eae
wyjzxl <36919429+wyjzxl@users.noreply.github.com>
2019-01-10 16:18:48 +0800

    work

--- CoustomerMode/src/main/Java/cn/com/guimei/dao/AdminMapper.java
+++
@@ -1,8 +0,0 @@
-package cn.com.guimei.dao;
-
-import cn.com.guimei.pojo.Superuser;
-import org.apache.ibatis.annotations.Param;
-
-public interface AdminMapper {
-    public Superuser adminLogin(@Param("userLoginName") String name,@Param("userPassword") String pwd);
-}
--- CoustomerMode/src/main/Java/cn/com/guimei/service/AdminService.java
+++
@@ -1,7 +0,0 @@
-package cn.com.guimei.service;
-
-import cn.com.guimei.pojo.Superuser;
-
-public interface AdminService {
-    public Superuser adminLogin(String userLogin,String userPassword);
-}
